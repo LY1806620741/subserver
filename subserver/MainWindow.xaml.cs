7d331c0cabaf24b2a440a27783dcb4de50b1e2d3
huangjunjie <942101897@qq.com>
2020-10-23 08:43:20 +0800

    添加项目文件。

---
+++ subserver/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using subserver.controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace subserver
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Server server=new Server();
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Switchclick(object sender, RoutedEventArgs e)
+        {
+            if (_switch.Content.ToString() != "关闭")
+            {
+                this._switch.Content = "关闭";
+                this.tips.Text= String.Join(",",server.GetIP().Select(ip => ip.ToString()).ToList());
+            }
+            else
+            {
+                this._switch.Content = "开始";
+                this.tips.Text = "IP";
+            }
+        }
+    }
+}
---
+++ subserver/Server.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace subserver.controller
+{
+    class Server
+    {
+
+        public System.Net.IPAddress[] GetIP()
+        {
+            string hostName = System.Net.Dns.GetHostName();
+            //hostName获取主机名称
+            System.Net.IPHostEntry ipEntry = System.Net.Dns.GetHostEntry(hostName);//Dns.GetHostByName(hostName);
+                                                                                   //ip地址列表
+            return  ipEntry.AddressList;
+        }
+    }
+}
